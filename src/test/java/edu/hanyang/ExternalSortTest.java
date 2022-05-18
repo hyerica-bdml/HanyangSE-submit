d93d37250523e59233a72807bc6774bab1fa0ef0
Lee, Jaeyoung <wayexists02@gmail.com>
2022-05-19 00:10:56 +0900

    BPlusTreeTest code added

--- src/test/java/edu/hanyang/ExternalSortTest.java
+++ src/test/java/edu/hanyang/ExternalSortTest.java
@@ -3,12 +3,12 @@ package edu.hanyang;
 
 import edu.hanyang.submit.HanyangSEExternalSort;
 import io.github.hyerica_bdml.indexer.ExternalSort;
+import java.lang.management.ManagementFactory;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
-import java.lang.management.ManagementFactory;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,8 +18,8 @@ public class ExternalSortTest {
     private static final String INPUT_DATA_PATH = "data/input_10000000.data";
     private static final String OUTPUT_DATA_PATH = "data/output_10000000.data";
     private static final String TEMP_DIR_PATH = "tmp/";
-    private static final int BLOCKSIZE = 1024*8;
-    private static final int N_BLOCKS = 1000;
+    private static int BLOCKSIZE = 1024*8;
+    private static int N_BLOCKS = 500;
 
     private static ExternalSort sort;
 
@@ -36,24 +36,33 @@ public class ExternalSortTest {
 
     @BeforeClass
     public static void prepare() throws IOException {
+        try {
+            BLOCKSIZE = Integer.parseInt(System.getenv("BLOCKSIZE"));
+            N_BLOCKS = Integer.parseInt(System.getenv("N_BLOCKS"));
+        } catch (Exception exc) {}
+
         if (Files.exists(Paths.get(TEMP_DIR_PATH)) && Files.isDirectory(Paths.get(TEMP_DIR_PATH))) {
             removeDir(Paths.get(TEMP_DIR_PATH));
         } else {
             Files.createDirectory(Paths.get(TEMP_DIR_PATH));
         }
 
+        if (Files.exists(Paths.get(OUTPUT_DATA_PATH)))
+            Files.deleteIfExists(Paths.get(OUTPUT_DATA_PATH));
+
         sort = new HanyangSEExternalSort();
     }
 
     @Test
     public void testSort() throws IOException {
+    	System.out.println("available memroy = " + 
+    	        ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax());
 
         long currentTime = System.currentTimeMillis();
         sort.sort(INPUT_DATA_PATH, OUTPUT_DATA_PATH, TEMP_DIR_PATH, BLOCKSIZE, N_BLOCKS);
         long duration = System.currentTimeMillis() - currentTime;
         System.out.println("Duration: " + ((double) duration / 1000.0));
-        System.out.println("available memroy = " + 
-    	        ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax());
+
         try (FileInputStream fin = new FileInputStream(OUTPUT_DATA_PATH);
              BufferedInputStream bin = new BufferedInputStream(fin);
              DataInputStream in = new DataInputStream(bin)) {
@@ -62,7 +71,8 @@ public class ExternalSortTest {
             int previousDocId = -1;
             int previousPos = -1;
 
-            int cnt = 0;
+            int cnt = 0
+
             while (in.available() > 0) {
                 int termId = in.readInt();
                 int docId = in.readInt();
@@ -79,8 +89,8 @@ public class ExternalSortTest {
                 previousTermId = termId;
                 previousDocId = docId;
                 previousPos = pos;
-                
-                if (cnt++ >= 20000) break;
+
+                if (cnt++ == 20000) break;
             }
         }
     }
